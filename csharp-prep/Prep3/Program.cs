0c6d7c871c395c39cc4f0754a3a0a8c8e4bd7fb7
frenzreal <frenz.real01@gmail.com>
2023-04-27 11:35:50 +0800

    Completed C# Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = -1;
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 11);
+        Console.WriteLine("Guess a number from 1 to 11");
+        while (guess != answer)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
