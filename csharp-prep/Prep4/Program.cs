2a88426faca10218b9c725f98bc05bff83b4c70e
frenzreal <frenz.real01@gmail.com>
2023-04-27 12:06:40 +0800

    Completed C# Prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int numb = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (numb != 0)
+        {
+            Console.Write("Enter a number: ");
+            numb = int.Parse(Console.ReadLine());
+            if  (numb != 0)
+            {
+                numbers.Add(numb);
+            }
+
+        }
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }
